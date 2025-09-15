f347c044e5b6e18a3be503af5fe66da32d812580
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-15 08:41:31 +0700

    refactor: split code func menuUtama and inputNama

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-import { input } from "./lib/interfaceInput.js";
-import { cariMenu } from "./lib/menu.js";
-import { lihatKeranjang } from "./lib/keranjang.js";
-import { showHistory } from "./lib/history.js";
-import { keluar } from "./lib/keluar.js";
-
-export let namaUser = "";
-
-const inputNama = async () => {
-  process.stdout.write("\x1Bc");
-  console.log("\n Selamat Datang di Aplikasi Pemesanan Mixue🤗\n");
-  namaUser = await input(" Masukkan nama anda: ", "nama");
-};
-
-const menuUtama = async () => {
-  let loop = true;
-  while (loop) {
-    process.stdout.write("\x1Bc");
-    console.log(`\nHalo ${namaUser} silahkan pilih menu\n`);
-    console.log("1. Cari Menu");
-    console.log("2. Lihat Keranjang");
-    console.log("3. Lihat History");
-    console.log("4. Keluar\n");
-
-    const inputMenu = await input("Masukkan nomor menu: ");
-
-    switch (inputMenu) {
-      case 1:
-        await cariMenu();
-        break;
-      case 2:
-        await lihatKeranjang();
-        break;
-      case 3:
-        await showHistory();
-        break;
-      case 4:
-        loop = await keluar(loop);
-        break;
-      default:
-        await input("\n *Input tidak sesuai yang diharapkan*");
-        break;
-    }
-  }
-};
+import { menuUtama, inputNama } from "./lib/menuUtama.js";
 
 const main = async () => {
   await inputNama();
